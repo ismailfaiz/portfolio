c73853593ef4059415dcefe9519a4970f491f775
Ismail Faiz <63803097+IsmailFaiz@users.noreply.github.com>
2024-04-21 09:24:44 -0700

    Chat implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
+import Chat from '@/components/chat'
+
+export const runtime = 'edge'
+
 export default function Home() {
-  return (
-    <section className='py-24'>
-      <div className='container'>
-        <h1 className='text-3xl font-bold'>Next TS Starter</h1>
-      </div>
-    </section>
-  )
+  return <Chat />
 }
